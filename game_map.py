83e3337bffcb01fc7805ad39cf07fde9da94d357
Stan Wielga <svwielga4@gmail.com>
2020-10-01 21:02:08 -0400

    fixing type

--- game_map.py
+++ game_map.py
@@ -6,7 +6,8 @@ import tile_types
 
 class GameMap:
     def __init__(self, width: int, height: int):
-        self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")l
+        self.width, self.height = width, height
+        self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside of the bounds of this map."""
